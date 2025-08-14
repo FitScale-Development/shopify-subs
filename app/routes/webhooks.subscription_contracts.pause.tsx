b07167aea0dc4e1fd6e3df1fcf953074a8a88fab
Michael Kanahele <michael@fitscale.net>
2025-08-14 13:48:41 -0500

    Test

--- app/routes/webhooks.subscription_contracts.pause.tsx
+++ app/routes/webhooks.subscription_contracts.pause.tsx
@@ -5,6 +5,23 @@ import {logger} from '~/utils/logger.server';
 export const action = async ({request}: ActionFunctionArgs) => {
   const {topic, shop, payload} = await authenticate.webhook(request);
 
+  const url = new URL('https://hydro-tracking.vercel.app/api/event');
+  url.searchParams.append('event', 'pause');
+  url.searchParams.append('path', '/');
+  url.searchParams.append('senderId', 'asdf');
+
+  try {
+    const res = await fetch(url.toString(), {
+      method: 'GET',
+    });
+
+    if (!res.ok) {
+      console.error('Failed to track event', res.status, await res.text());
+    }
+  } catch (error) {
+    console.error('Error tracking event:', error);
+  }
+
   logger.info({topic, shop, payload}, 'Received webhook');
 
   return new Response();
